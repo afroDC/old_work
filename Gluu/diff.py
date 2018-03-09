35225a7309e1285e1b605f0dff38b56799b8a472
Chris B <afroappdev@gmail.com>
2018-03-09 15:24:55 -0600

    Update diff.py

--- Gluu/diff.py
+++ Gluu/diff.py
@@ -9,20 +9,8 @@ file2="idp2.ldif"
 ld1 = ldif.LDIFRecordList.parse(file1)
 ld2 = ldif.LDIFRecordList.parse(file2)
 
-'''
-d1_lines = open(file1,"r").readlines()
-d2_lines = open(file2,"r").readlines()
-
-for line in d1_lines:
-    if line in d2_lines:
-        continue
-    elif line not in d2_lines:
-        print(line)
-'''
-
 for line in ld1:
     if line in ld2:
         continue
     elif line not in ld2:
         print(LDIFWriter.unparse(line))
-
