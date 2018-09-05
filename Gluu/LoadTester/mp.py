ab1b29e7d5642a6d433f749606c27e3570ed48e3
Chris B <afroappdev@gmail.com>
2018-09-04 20:09:21 -0500

    Create mp.py

---
+++ Gluu/LoadTester/mp.py
@@ -0,0 +1,63 @@
+import multiprocessing as mp
+import random
+import string
+import time
+from ldap3 import Server, Connection, MODIFY_REPLACE, MODIFY_ADD, MODIFY_DELETE, SUBTREE, ALL, BASE, LEVEL
+from random import randint
+
+import loadtester
+
+# Gather the organizational inum  once so we don't waste cycles gathering it again.
+
+
+# Change to your host
+host = "example.gluu.org:1636"
+
+# Input LDAP DN and Password in the following variables
+password = 'example'
+
+# 'cn=directory manager,o=gluu' for OpenLDAP
+DN = 'cn=directory manager'
+
+login = 'https://example.gluu.org/identity/home.htm'
+logout = 'https://example.gluu.org/identity/logout'
+
+testuser = 'example'
+password = 'secret'
+userlist = []
+
+oInum = loadtester.getoInum()
+
+def getUserList():
+    print("Gathering all test users into memory.")
+    try:
+        ldap_uri = "ldaps://{}".format(host)
+        server = Server(ldap_uri, use_ssl=True)
+        conn = Connection(server, DN, password)
+        conn.bind()
+        print('Connected.')
+
+        searchBase = 'ou=people,o={},o=gluu'.format(oInum)
+        conn.search(search_base = searchBase,
+        search_filter = '(uid={}*)'.format(testuser),
+        search_scope = SUBTREE,
+        attributes=['uid'])
+        for entry in conn.entries:
+            userlist.append(entry['uid'])
+        return userlist
+    except Exception as ex:
+        print("Cannot connect to host.")
+        print(ex)
+
+userlist = getUserList()
+
+def mp_loadtest():
+    for x in range(10):
+        username = str(userlist[randint(0, len(userlist)-1)]) 
+        p = mp.Process(target=loadtester.loadtest, args=(username,))
+        p.start()
+    time.sleep(randint(0,5))
+    mp_loadtest()
+
+if __name__ == '__main__':
+    mp_loadtest()
