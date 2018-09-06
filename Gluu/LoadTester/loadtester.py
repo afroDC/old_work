a7dc788a066f0b62708d516528b1444b43436707
Chris B <afroappdev@gmail.com>
2018-09-06 12:50:57 -0500

    Add logging and cleanup

--- Gluu/LoadTester/loadtester.py
+++ Gluu/LoadTester/loadtester.py
@@ -5,13 +5,12 @@ from random import randint
 from base64 import b64encode
 from ldap3 import Server, Connection, MODIFY_REPLACE, MODIFY_ADD, MODIFY_DELETE, SUBTREE, ALL, BASE, LEVEL
 
-urllib3.disable_warnings()
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 login = 'https://example.gluu.org/identity/home.htm'
 logout = 'https://example.gluu.org/identity/logout'
 
-testuser = 'example'
-password = 'secret'
+userpassword = 'secret'
 userlist = []
 
 def getoInum(host,password,DN):
@@ -34,24 +33,40 @@ def getoInum(host,password,DN):
         print(ex)
 
 def loadtest(username):
-
+    # Build session states
     r = requests.get(login, verify=False)
-
     if r.status_code == 200:
-        print('Initial request successful.')
-        time.sleep(1) 
-    else:
-        print r.status_code
+        #print('Initial request successful.')
 
-    r = requests.get(login, auth=(username,password), verify=False)
-
-    if r.status_code == 200:
-        print('Logged in as {}'.format(username))
-        r = requests.get(logout, verify=False)
+        # Log user in
+        r = requests.get(login, auth=(username,userpassword), verify=False)
         if r.status_code == 200:
-            print('Logged out as {}'.format(username))
+            #print('Logged in as {}'.format(username))
+            loadlog = open("activity.log", "a")
+            loadlog.write("{} [INFO] {} {} logged in.\n".format(time.asctime( time.localtime(time.time()) ), str(r.status_code), username))
+            loadlog.close()
+            # Log user out
+            r = requests.get(logout, verify=False)
+            if r.status_code == 200:
+                #print('Logged out as {}'.format(username))
+                loadlog = open("activity.log", "a")
+                loadlog.write("{} [INFO] {} {} logged out.\n".format(time.asctime( time.localtime(time.time()) ), str(r.status_code), username))
+                loadlog.close()
+            else:
+                print("{} [ERROR] {} Logout failed. Please check error.log.".format(time.asctime( time.localtime(time.time()) ), str(r.status_code)))
+                loadErrorlog = open("error.log", "a")
+                loadErrorlog.write("{} [ERROR] {} Logout failed for user {}\n".format(time.asctime( time.localtime(time.time()) ), str(r.status_code), username))
+                loadErrorlog.write("{} [FAILED] \n{}\n{}\n".format(time.asctime( time.localtime(time.time()) ), str(r.reason), str(r.text)))
+                loadErrorlog.close()
         else:
-            print r.status_code
+            print("{} [ERROR] {} Login failed. Please check error.log.".format(time.asctime( time.localtime(time.time()) ), str(r.status_code)))
+            loadErrorlog = open("activity.log", "a")
+            loadErrorlog.write("{} [ERROR] {} Login failed for user {}\n".format(time.asctime( time.localtime(time.time()) ), str(r.status_code), username))
+            loadErrorlog.write("{} [FAILED] \n{}\n{}\n".format(time.asctime( time.localtime(time.time()) ), str(r.reason), str(r.text)))
+            loadErrorlog.close()
     else:
-        print('Login failed.')
-        print r.status_code
+        print("{} [ERROR] {} Session connection failed. Please check error.log.".format(time.asctime( time.localtime(time.time()) ), str(r.status_code)))
+        loadErrorlog = open("error.log", "a")
+        loadErrorlog.write("{} [ERROR] {}\n".format(time.asctime( time.localtime(time.time()) ), str(r.status_code)))
+        loadErrorlog.write("{} [FAILED] \n{}\n{}\n".format(time.asctime( time.localtime(time.time()) ), str(r.reason), str(r.text)))
+        loadErrorlog.close()
